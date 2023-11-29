9b342200a9e390ed9083efd364987d666f17fa84
Kyle Francis Edison Orollo Atienza <Kyle.Atienza@EntainGroup.com>
2023-11-30 03:48:42 +0800

    update

--- script.js
+++ script.js
@@ -13,11 +13,27 @@ const enumerateArrayToString = (array) => {
     return arrayToString.join(' ')
 }
 
+const createToast = (label, ...classes) => {
+    const newToast = document.createElement('div')
+    newToast.classList.add('alert', 'alert-info', 'opacity-100', 'transition', 'border-0', ...classes)
+    newToast.innerHTML = `<span>${label}</span>`
+    toast.appendChild(newToast)
+
+    setTimeout(() => {
+        // toast.removeChild(newToast)
+        newToast.classList.add('opacity-0')
+    }, 2000)
+}
+
 const getPublishingUpdate = (startDate, endDate, reccurenceDays) => {
     let update = ''
 
-    if (startDate && endDate) {
-        update += `This promotion is now scheduled to run from <b>${new Date(startDate).toDateString()} to ${new Date(endDate).toDateString()}</b>`
+    if (startDate) {
+        if (!endDate) {
+            update += `This promotion is now scheduled to run from <b>${new Date(startDate).toDateString()} without an end date</b>`
+        } else {
+            update += `This promotion is now scheduled to run from <b>${new Date(startDate).toDateString()} to ${new Date(endDate).toDateString()}</b>`
+        }
 
         if (reccurenceDays.length) {
             update += ` on <b>${enumerateArrayToString(reccurenceDays)}</b>`
@@ -41,6 +57,9 @@ getUpdate.addEventListener('click', () => {
     const filter = document.getElementById('filter').value
     const startDate = document.getElementById('startDate').value
     const endDate = document.getElementById('endDate').value
+
+    const needFinalPat = document.getElementById('needFinalPat').checked
+    const needFilter = document.getElementById('needFilter').checked
     const addedToCarouselSheet = document.getElementById('addedToCarouselSheet').checked
 
     const affectedComponents = [...document.querySelectorAll('.affectedComponents')]
@@ -54,16 +73,22 @@ getUpdate.addEventListener('click', () => {
     updateHtml += `<p>`
     updateHtml += getPublishingUpdate(startDate, endDate, reccurenceDays)
     updateHtml += ` `
-    if (affectedComponents.length) {
+    if (needFilter) {
         if (filter) {
-            updateHtml += `<span>I have used the filter <b>${filter}</b> for the <b>${enumerateArrayToString(affectedComponents)}</b>.</span>`
+            if (affectedComponents.length) {
+                updateHtml += `<span>I have used the filter <b>${filter}</b> for the <b>${enumerateArrayToString(affectedComponents)}</b>.</span>`
+            } else {
+                updateHtml += `<span>I have used no filter for the <b>${enumerateArrayToString(affectedComponents)}</b>.</span>`
+            }
         } else {
-            updateHtml += `<span>I have used no filter for the <b>${enumerateArrayToString(affectedComponents)}</b>.</span>`
+            createToast('Please add filter', 'bg-warning')
         }
     }
     updateHtml += ` `
-    if (finalPat) {
-        updateHtml += `<span>The MPP is now updated with the provided <b>final PAT ${finalPat}</b>.</span>`
+    if (needFinalPat) {
+        if (finalPat) {
+            updateHtml += `<span>The MPP is now updated with the provided <b>final PAT ${finalPat}</b>.</span>`
+        }
     }
     updateHtml += ` `
     if (addedToCarouselSheet) {
@@ -71,9 +96,9 @@ getUpdate.addEventListener('click', () => {
     }
     updateHtml += `</p>`
 
-
-    if (!finalPat) {
-        updateHtml += `<br><p>Final PAT is not yet provided. </p>`
+    updateHtml += `<br>`
+    if (needFinalPat && !finalPat) {
+        updateHtml += `<p>Final PAT is not yet provided. </p>`
     }
 
     updateContainer.innerHTML = updateHtml
@@ -90,14 +115,5 @@ copyUpdate.addEventListener('click', () => {
 
     navigator.clipboard.write(clipboard)
 
-    const copyAlert = document.createElement('div')
-    copyAlert.classList.add('alert', 'alert-info', 'bg-primary', 'text-white', 'opacity-100', 'transition')
-    copyAlert.innerHTML = `<span>Update Copied</span>`
-    toast.appendChild(copyAlert)
-
-    setTimeout(() => {
-        // toast.removeChild(copyAlert)
-        copyAlert.classList.add('opacity-0')
-    }, 2000)
-
+    createToast('Update Copied', 'bg-primary', 'text-white')
 })
